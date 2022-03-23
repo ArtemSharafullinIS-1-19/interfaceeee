ea392c6757dd9b69337fdac7bf7209463c1acf12
Артём шарафуллин <rejs03@mail.ru>
2022-03-23 15:26:39 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Интерфейсы
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.введитеИмяToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.введитеАдресToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox2 = new System.Windows.Forms.ToolStripTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 28);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(364, 164);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton2,
+            this.toolStripButton1,
+            this.toolStripButton3});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(364, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(75, 22);
+            this.toolStripButton2.Text = "Арендовать";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(129, 22);
+            this.toolStripButton1.Text = "Загрузить документы";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton3.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.введитеИмяToolStripMenuItem,
+            this.введитеАдресToolStripMenuItem});
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(80, 22);
+            this.toolStripButton3.Text = "Настройки";
+            // 
+            // введитеИмяToolStripMenuItem
+            // 
+            this.введитеИмяToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1});
+            this.введитеИмяToolStripMenuItem.Name = "введитеИмяToolStripMenuItem";
+            this.введитеИмяToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            this.введитеИмяToolStripMenuItem.Text = "Введите имя";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 23);
+            // 
+            // введитеАдресToolStripMenuItem
+            // 
+            this.введитеАдресToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox2});
+            this.введитеАдресToolStripMenuItem.Name = "введитеАдресToolStripMenuItem";
+            this.введитеАдресToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            this.введитеАдресToolStripMenuItem.Text = "Введите адрес";
+            // 
+            // toolStripTextBox2
+            // 
+            this.toolStripTextBox2.Name = "toolStripTextBox2";
+            this.toolStripTextBox2.Size = new System.Drawing.Size(100, 23);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 192);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripButton3;
+        private System.Windows.Forms.ToolStripMenuItem введитеИмяToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem введитеАдресToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿    using System;
+using System.Windows.Forms;
+
+namespace Интерфейсы
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        class Info //хранение информации методов
+        {
+            //без статиков не работает :(
+            public static string id_selected_rows = "0";
+            public static bool Employee;
+            public static bool check = false;
+        }
+        interface IPerson //Создание интерфейса Person
+        {
+            //
+            string Name { get; set; }
+            string Address { get; set; }
+            void Renc()
+            {
+                MessageBox.Show("Аккаунт не подтвержден. Загрузите документы.", "Уведомление");
+            }
+        }
+        class Person
+        {
+            public IPerson PersonInfo { get; set; } //свойство которое будет хранить экземпляры классов
+        }
+
+        public class Employees : IPerson
+        {
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public Employees(string _name, string _address)
+            {
+                Name = _name;
+                Address = _address;
+            }
+            public void Renc()
+            {
+                MessageBox.Show("Вы являетесь сотрудником, не пытайтесь это сделать.", "Уведомление");
+            }
+        }
+
+        class User : IPerson
+        {
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public User(string _name, string _address)
+            {
+                Name = _name;
+                Address = _address;
+            }
+        }
+        class ConfirmUser : User, IPerson
+        {
+            public ConfirmUser(string _name, string _address) : base(_name, _address)
+            {
+                Name = _name;
+                Address = _address;
+            }
+            public void Renc()
+            {
+                MessageBox.Show($"Аренда мотоцикла {Info.id_selected_rows} прошла успешно!", "Уведомление");
+            }
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            
+            if (toolStripTextBox1.Text != "" && toolStripTextBox2.Text != "") { Info.check = true; MessageBox.Show("Вы успешно подтвердили свой аккаунт!"); }
+            else MessageBox.Show("Введите данные в настройках");
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            //апкаст
+            Person person = new Person();
+            if(!Info.Employee) //проверка данных, считывает сотрудников, и пользователей
+            {
+                if (!Info.check) person.PersonInfo = new User(toolStripTextBox1.Text, toolStripTextBox2.Text);
+                else person.PersonInfo = new ConfirmUser(toolStripTextBox1.Text, toolStripTextBox2.Text);
+            }
+            else person.PersonInfo = new Employees(toolStripTextBox1.Text, toolStripTextBox2.Text);
+            GetSelectedIDString();
+            person.PersonInfo.Renc(); //один вызов для 3-ех классов
+        }
+
+        //Метод получения ID выделенной строки, для последующего вызова его в нужных методах
+        public void GetSelectedIDString()
+        {
+            //Переменная для индекс выбранной строки в гриде
+            string index_selected_rows;
+            //Индекс выбранной строки
+            index_selected_rows = dataGridView1.SelectedCells[0].RowIndex.ToString();
+            //ID конкретной записи в Базе данных, на основании индекса строки
+            Info.id_selected_rows = dataGridView1.Rows[Convert.ToInt32(index_selected_rows)].Cells[0].Value.ToString();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //заполнение datagrid
+            DataGridViewTextBoxColumn col0 = new DataGridViewTextBoxColumn();
+            col0.HeaderText = "Марка";
+            col0.Name = "ID1";
+            DataGridViewTextBoxColumn col1 = new DataGridViewTextBoxColumn();
+            col1.HeaderText = "Модель";
+            col1.Name = "ID2";
+            DataGridViewTextBoxColumn col2 = new DataGridViewTextBoxColumn();
+            col2.HeaderText = "Max Speed";
+            col2.Name = "ID3";
+            this.dataGridView1.Columns.Add(col0);
+            this.dataGridView1.Columns.Add(col1);
+            this.dataGridView1.Columns.Add(col2);
+
+            DataGridViewCell cel0 = new DataGridViewTextBoxCell();
+            DataGridViewCell cel1 = new DataGridViewTextBoxCell();
+            DataGridViewCell cel2 = new DataGridViewTextBoxCell();
+            DataGridViewRow row = new DataGridViewRow();
+            cel0.Value = "Honda";
+            cel1.Value = "FireStorm";
+            cel2.Value = "176";
+            row.Cells.AddRange(cel0, cel1, cel2);
+            this.dataGridView1.Rows.Add(row);
+            cel0 = new DataGridViewTextBoxCell();
+            cel1 = new DataGridViewTextBoxCell();
+            cel2 = new DataGridViewTextBoxCell();
+            row = new DataGridViewRow();
+            cel0.Value = "SUZUKI";
+            cel1.Value = "GSX-R1000";
+            cel2.Value = "190";
+            row.Cells.AddRange(cel0, cel1, cel2);
+            this.dataGridView1.Rows.Add(row);
+            cel0 = new DataGridViewTextBoxCell();
+            cel1 = new DataGridViewTextBoxCell();
+            cel2 = new DataGridViewTextBoxCell();
+            row = new DataGridViewRow();
+            cel0.Value = "BMW";
+            cel1.Value = "S1000RR";
+            cel2.Value = "200+";
+            row.Cells.AddRange(cel0, cel1, cel2);
+            this.dataGridView1.Rows.Add(row);
+            cel0 = new DataGridViewTextBoxCell();
+            cel1 = new DataGridViewTextBoxCell();
+            cel2 = new DataGridViewTextBoxCell();
+            row = new DataGridViewRow();
+            cel0.Value = "YAMAHA";
+            cel1.Value = "YZF-R6";
+            cel2.Value = "187";
+            row.Cells.AddRange(cel0, cel1, cel2);
+            this.dataGridView1.Rows.Add(row);
+            //msgbox с получением инфы
+            if (MessageBox.Show("Вы сотрудник?", "Вопрос", MessageBoxButtons.YesNo) == DialogResult.Yes) Info.Employee = true;
+            else Info.Employee = false;
+        }
+    }
+}
